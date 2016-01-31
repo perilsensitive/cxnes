e56689bf69bbb824d37150847d477426ccfb5589
Ryan Jackson <perilsensitive@gmail.com>
2016-01-30 23:15:58 -0700

    FDS fixes

--- boards/fds.c
+++ boards/fds.c
@@ -372,7 +372,7 @@ static int fds_init(struct board *board)
 			     fds_bios_misc);
 	cpu_set_read_handler(emu->cpu, 0xefaf, 1, 0,
 			     fds_bios_misc);
-	cpu_set_read_handler(emu->cpu, 0xeeb8, 1, 0,
+	cpu_set_read_handler(emu->cpu, 0xeedf, 1, 0,
 			     fds_bios_misc);
 	cpu_set_read_handler(emu->cpu, 0xe682, 1, 0,
 			     fds_bios_misc);
@@ -1436,11 +1436,10 @@ static CPU_READ_HANDLER(fds_bios_misc)
 			value = cpu_peek(emu->cpu, 0xefcd);
 		}
 		break;
-	case 0xeeb8:
-		/* Disable the self-test check, initial title screen
-		   and check for disk presence (will automatically
-		   insert disk at reset (hard or soft) and select disk
-		   1, side A))
+	case 0xeedf:
+		/* Disable the initial title screen check for disk
+		   presence (will automatically insert disk at reset
+		   (hard or soft) and select disk 1, side A).
 		*/
 		if (emu->config->fds_hide_bios_title_screen) {
 			cpu_set_pc(emu->cpu, 0xef46);
