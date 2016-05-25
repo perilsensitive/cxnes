1efd6477d807ffe627b98cc03207420d5a610664
Ryan Jackson <perilsensitive@gmail.com>
2016-05-25 05:41:29 -0700

    More window resizing fixes

--- sdl/sdl_video.c
+++ sdl/sdl_video.c
@@ -85,7 +85,7 @@ int center_x, center_y;
 int mouse_grabbed;
 int crosshairs_enabled;
 
-static int scaling_factor = 1;
+static int device_scaling_factor = 1;
 static uint32_t rgb_palette[512];
 uint32_t *nes_screen;
 static int nes_screen_width = 256;
@@ -805,8 +805,11 @@ int video_apply_config(struct emu *emu)
 		width_stretch = PAL_WIDTH_STRETCH;
 	}
 
-	window_rect.w = view_w * width_stretch * window_scaling_factor;
-	window_rect.h = view_h * window_scaling_factor;
+	if (window_rect.w < (view_w * width_stretch * window_scaling_factor))
+		window_rect.w = view_w * width_stretch * window_scaling_factor;
+
+	if (window_rect.h < (view_h * window_scaling_factor))
+		window_rect.h = view_h * window_scaling_factor;
 
 	osd_enabled = emu->config->osd_enabled;
 
@@ -837,6 +840,7 @@ int video_apply_config(struct emu *emu)
 	if (gui_enabled)
 	{
 		gui_set_size(window_rect.w, window_rect.h);
+		handle_resize_event();
 	}
 	else
 #endif
@@ -1576,7 +1580,8 @@ void video_set_window_title(const char *title)
 void video_resize(int w, int h)
 {
 	if (window)
-		SDL_SetWindowSize(window, w * scaling_factor, h * scaling_factor);
+		SDL_SetWindowSize(window, w * device_scaling_factor,
+				  h * device_scaling_factor);
 }
 
 /* Force a redraw of the current buffer.  Useful if
@@ -1627,8 +1632,8 @@ void video_mouse_button(int button, int state, int x, int y)
 
 	sdlevent.button.which = 0;
 	sdlevent.button.windowID = SDL_GetWindowID(window);
-	sdlevent.button.x = x * scaling_factor;
-	sdlevent.button.y = y * scaling_factor;
+	sdlevent.button.x = x * device_scaling_factor;
+	sdlevent.button.y = y * device_scaling_factor;
 	sdlevent.button.button = button;
 	sdlevent.button.clicks = 1;
 
@@ -1647,11 +1652,11 @@ void video_mouse_motion(int x, int y, int button_state)
 	sdlevent.type = SDL_MOUSEMOTION;
 	sdlevent.motion.which = 0;
 	sdlevent.button.windowID = SDL_GetWindowID(window);
-	sdlevent.motion.x = x * scaling_factor;
-	sdlevent.motion.y = y * scaling_factor;
+	sdlevent.motion.x = x * device_scaling_factor;
+	sdlevent.motion.y = y * device_scaling_factor;
 	sdlevent.motion.state = button_state;
-	sdlevent.motion.xrel = (x - mouse_lastx) * scaling_factor;
-	sdlevent.motion.yrel = (y - mouse_lasty) * scaling_factor;
+	sdlevent.motion.xrel = (x - mouse_lastx) * device_scaling_factor;
+	sdlevent.motion.yrel = (y - mouse_lasty) * device_scaling_factor;
 	mouse_lastx = x;
 	mouse_lasty = y;
 
@@ -1694,7 +1699,7 @@ void video_restored(void)
 
 void video_set_scaling_factor(int factor)
 {
-	scaling_factor = factor;
+	device_scaling_factor = factor;
 }
 
 void video_get_windowed_size(int *w, int *h)
